c2b1c60b7700018631328e563f0f08c73bcaa02d
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:49:14 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan11/Formating.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan11;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : IF-1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program Formatting
+ * class
+ * 
+ */
+public class Formating {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+}
